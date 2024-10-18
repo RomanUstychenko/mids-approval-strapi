d1a153f9ff170fe2d131601e1b1e7ad0bd89d232
Roman <roman.ustychenko@gmail.com>
2024-10-18 20:01:11 +0300

    comit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '28c2ee817c5e339c3940220cd694fde7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cb811426-43b4-49e3-8e69-5195297cbabe'
+};
